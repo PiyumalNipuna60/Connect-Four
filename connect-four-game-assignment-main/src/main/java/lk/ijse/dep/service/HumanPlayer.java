16396e99ef9a6fd63703784081905da75024efb8
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-19 01:32:21 +0530

    Add HumanPlayer class

---
+++ connect-four-game-assignment-main/src/main/java/lk/ijse/dep/service/HumanPlayer.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep.service;
+
+public class HumanPlayer extends Player{
+
+    public HumanPlayer(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public void movePiece(int col){}
+}
